45646015d12d5ff217367569057e0a437cf2e02d
nikolaeva2 <lavegirle21@gmail.com>
2023-03-19 05:47:33 +0300

    второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Надя - разработчик");
+        System.out.println("Ты -лучший разработчик!");
+          System.out.println("У тебя всё получается!");
     }
 }
